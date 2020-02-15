0286496eed03eb09c25ba9e88dfccad34023dd7e
Chaitanya <chaitanyas0101@gmail.com>
2020-02-15 08:10:40 +0530

    added few codes

--- app/src/main/java/com/example/notify/MainActivity.java
+++ app/src/main/java/com/example/notify/MainActivity.java
@@ -1,5 +1,8 @@
 package com.example.notify;
 
+import android.app.NotificationManager;
+import android.app.PendingIntent;
+import android.content.Intent;
 import android.os.Bundle;
 
 import com.google.android.material.floatingactionbutton.FloatingActionButton;
@@ -7,6 +10,7 @@ import com.google.android.material.snackbar.Snackbar;
 
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
+import androidx.core.app.NotificationCompat;
 
 import android.view.View;
 import android.view.Menu;
@@ -18,38 +22,20 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        Toolbar toolbar = findViewById(R.id.toolbar);
-        setSupportActionBar(toolbar);
 
-        FloatingActionButton fab = findViewById(R.id.fab);
-        fab.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
-            }
-        });
-    }
+        NotificationCompat.Builder builder=new android.support.v4.app.NotificationCompact.Builder(context:this);
+        builder.setSmallIcon(R.drawable.ic_launcher_background);
+                builder.setContentTitle("whatsappmessage");
+        builder.setContentText("message is hi how r u");
 
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.menu_main, menu);
-        return true;
-    }
+        Intent intent=new Intent(packageContext:this, ResultActivity.class);
+        PendingIntent pendingIntent = pendingIntent.getActivity(context:this,requestCode:0,intent,PendingIntent.FLAG_UPDATE_CURRENT);
+        builder.setContentIntent(pendingIntent);
 
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
+        NotificationManager manager=(NotificationManager) getSystemService(NOTIFICATION_SERVICE ) manager.notify( id:1, builder.build());
 
-        //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
-            return true;
-        }
 
-        return super.onOptionsItemSelected(item);
+
+
     }
 }
